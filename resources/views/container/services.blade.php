aca49847442f4c541019b82852103c12857c44e2
aurafh <aurafitriahazar@gmail.com>
2024-03-30 08:30:36 +0700

    init commit

---
+++ resources/views/container/services.blade.php
@@ -0,0 +1,36 @@
+<!-- Services-->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">What I Do</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="my-3">Watching Anime</h4>
+                <p class="text-muted">I'd like to watching anime since I was kid. Isekai is one of my fav genre in anime that I watch a lot</p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="my-3">Reading Comic</h4>
+                <p class="text-muted">Reading comic either Manhwa or Manga is the best thing that I always do everyday</p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                    <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 class="my-3">Listening Music</h4>
+                <p class="text-muted">Listening to the music is one of my hobby that I do a lot, everyday and anytime</p>
+            </div>
+        </div>
+    </div>
+</section>
+
